a2245492e6195353d92c9604171084c6c3bc46bb
punsoca <punsoca@gmail.com>
2022-02-15 17:56:08 -0800

    Update threading method and output files
    Create a new function 'do_something_with_thread_name' that dynamically
    assigns a name to each thread using "threading.currentThread().getName()".
    
    Also updated the decorator function "funcname()" to add a print statement
    to indicate that the decorated function completes execution.
    
    Updated the threading_method_output.txt to reflect changes in the output
    made by the code changes indicated above.

--- multi-threading/threading_methods.py
+++ multi-threading/threading_methods.py
@@ -13,7 +13,10 @@ def total_time(started):
     finished = time.perf_counter()
     return f'Total execution time is {round(finished-started,2)} second(s)'
 
-# the do_xxxxxxxx functions use time.sleep to test the different multi-threading methods
+########################################################################################
+# the following functions use time.sleep to test the different multi-threading methods
+########################################################################################
+
 def do_something():
     print('Sleeping one second...')
     time.sleep(1)
@@ -24,29 +27,39 @@ def do_something2(seconds):
     time.sleep(seconds)
     return f'Done sleeping {seconds} seconds.'
 
+# This function showcases the ability to assign a name to EACH thread dynamically using "threading.currentThread().getName()"
+# to identify WHICH thread starts and done sleeping in choronological order - demonstrating that a thread that starts first
+# DOES NOT NECESSARILY finish first before the others.
+def do_something_with_thread_name():
+    print( threading.currentThread().getName(), ' sleeping one second...')
+    time.sleep(1)
+    print( threading.currentThread().getName(), ' is done sleeping.')
+
 def do_futures(seconds):
     print(f'Sleeping {seconds} second(s)...')
     time.sleep(seconds)
     return f'Done sleeping {seconds} seconds.'
 
-# decorator function 'funcname' to print the name of the decorated function
+# decorator function 'funcname' is created solely for the purpose of printing the name of the decorated function as it executes
 def funcname(func):
     def wrapper_function(*args, **kwargs):
-        print(f'\n... Function "{func.__name__}()" ...')
-        return func(*args, **kwargs)
+        print(f'\n... Executing Function "{func.__name__}()" ...\n')
+        func(*args, **kwargs)
+        print(f'\n... Function "{func.__name__}()" completed!\n')
 
-    return wrapper_function   # function without () indicates returning the function as an argument without executing it
+    return wrapper_function
 
 
+#############################################################################
+# ALL eight (8) multi-threading methods below are decorated with @funcname
+#############################################################################
 
-# ALL eight (8) multi-threading methods are decorated with @funcname
 # --------------------------------- 1. call function twice without threading ---------------------------------
 @funcname
 def call_func_twice_no_threading():
     '''
     calling the function twice result in the process running for a whole two seconds - this is because this code waits for
     the first function call to complete before executing the function the second time (total of 2 seconds execution time)
-
     '''
 
     start = start_timer()
@@ -55,7 +68,7 @@ def call_func_twice_no_threading():
     do_something()
     do_something()
 
-    print(f'{total_time(start)}\n')
+    print(f'{total_time(start)}')
 
 # --------------------------------- 2.  execute a function using threading ---------------------------------
 @funcname
@@ -81,7 +94,7 @@ def run_two_threads_async():
 
     # the following line with be printed even while our thread are in sleep mode
     print('Finish executing our two threads')
-    print(f'{total_time(start)}\n')
+    print(f'{total_time(start)}')
     time.sleep(3)
 
 # --------------------------------- 3.  execute threading function with .join() method ---------------------------------
@@ -90,41 +103,36 @@ def run_two_threads_with_join():
     '''
         use the '.join()' method to instruct the threading function to finish execution before moving on
         to the rest of the script.
-
     '''
-# Requirement:  this requires the 'import threading' statement to execute do_something(). This multi-threading function
-# uses the Thread.join() in addition to Thread.start().  Expected total execution time for both threads is ~1 second.
 #
-# Unlike the run_two_threads_async() function - this time, with the Thread.join() method implemented, python waits for
-# the completion of the two threads before it runs the "Finishing executing..." and printing total execution time.
-#
-
     start = start_timer()
     # define the two threads - pass the function name we wish to execute to the 'target' parameter
     t1 = threading.Thread(target=do_something)
     t2 = threading.Thread(target=do_something)
 
-    # to run the thread objects  we need to do a start() method on each thread
-    # EXPECTATION: Finish time still 0 seconds because python started both threads and while the threads are sleeping, our script ran
-    # .............concurrently and continued on with the rest of the script - immediately coming down and calculate our finish time.
+    # We run a start() method on each thread to start the threads like we did in run_two_threads_async() function
     t1.start()
     t2.start()
 
-    # use the threading 'join()' method to specify that we want our threads to finish before contiuing running the rest of the script
+    #  This time, however we also add a call to the .join() method for each of the two threads.  Using the Thread's .join() method
+    #  instructs python to WAIT until all threads finish execution before running the "finish executing" print statement and beyond.
     t1.join()
     t2.join()
 
     print('Finish executing our two threads with .join() method')
-    print(f'{total_time(start)}\n')
+    print(f'{total_time(start)}')
     time.sleep(2)
 
 # --------------------------------- 4. using a thread list to run multiple threads asynchronously ---------------------------------
 @funcname
 def run_ten_threads_with_threadlist():
     '''
-        This function runs 10 THREADS asynchronously USING for-loop - this is a better way to create multiple threads, 
-        BUT this requires the multiple threads to be added to a thread list, with the explanation provided in the code 
+        This function runs 10 THREADS asynchronously USING for-loop - this is a better way to create multiple threads,
+        BUT this requires the multiple threads to be added to a thread list, with the explanation provided in the code
         comments below.
+
+        ADDITIONAL FEATURE: this method calls a target function (do_something_with_thread_name) that prints the thread name
+        that is DYNAMICALLY assigned to each thread to help identify which thread starts and done sleeping in chronological order.
     '''
 
     start = start_timer()
@@ -132,20 +140,20 @@ def run_ten_threads_with_threadlist():
 
     # create 10 threads within our for- loop
     for _ in range(10):
-        t = threading.Thread(target=do_something)
+        t = threading.Thread(target=do_something_with_thread_name)
         # every time a thread is created, it should be started by calling  .start() method on the thread object
         t.start()
 
-        # NOTE: You CANNOT put a "t.join()" within the same for-loop as the "t.start()", threads would behave as if they are run synchronously!
-        # When creating thread using a loop, we need to create a thread list and join each thread to the list.
+        # NOTE: DO NOT execute a join() WITHIN THE SAME for-loop as the start(), as threads would behave as if they are run SYNCHRONOUSLY!
+        # When creating MULTIPLE threads using a loop, we NEED TO CREATE A THREAD LIST and join each thread to the list.
         threads.append(t)
 
-    # after the threads are added to the thread list, we need a SEPARATE for- loop to execute the .join() method for each thread in the
-    # thread list.  Doing this allows python to WAIT UNTIL ALL THREADS ARE COMPLETED before python runs the "total_time" print statement.
+    # THE THREAD LIST created from the previous for- loop is used primarily to execute the .join() method for each thread in the list.
+    # Use a stand-alone for- loop to do this.  This is the right way to implement join method for multiple threads created by the previous for- loop.
     for thread in threads:
         thread.join()
 
-    print(f'{total_time(start)}\n')
+    print(f'{total_time(start)}')
 
 # BEGINNING PYTHON 3.2, the 'concurrent.futures' can be used in place of threading without the need to do a .start()
 # and join() on inividual threads.
@@ -192,7 +200,7 @@ def conc_futures_threading():
     print(f5.result())
 
 
-    print(f'{total_time(start)}\n')
+    print(f'{total_time(start)}')
 
 # ----------------------- 6.  write concurrent futures ThreadPool as context manager  -----------------------
 @funcname
@@ -219,19 +227,19 @@ def conc_futures_thrd_context_mgr():
         print(f5.result())
 
 
-    print(f'{total_time(start)}\n')
+    print(f'{total_time(start)}')
 
 # ------------------ 7.  using concurrent futures to run mutiple threads with as_completed ------------------
 @funcname
 def conc_futures_thrd_as_completed():
     '''
-        This function uses the concurrent.futures 'as_completed()' method. It is implemented in the for- loop 
-        where the results are to be printed in the order "as they are completed". 
-        
+        This function uses the concurrent.futures 'as_completed()' method. It is implemented in the for- loop
+        where the results are to be printed in the order "as they are completed".
+
         Meanwhile, the executor.submit is written differently, this time as a list comprehension, with the
         number of futures objects equal to the length of the secs list - "secs" list contains the list of numbers
-        corresponds to the time.sleep() value for each future object that the executor.submit() creates in the 
-        ist cmprehension.
+        corresponds to the time.sleep() value for each future object that the executor.submit() creates in the
+        list cmprehension.
 
     '''
 
@@ -244,16 +252,16 @@ def conc_futures_thrd_as_completed():
         for f in concurrent.futures.as_completed(results):
             print(f.result())
 
-    print(f'{total_time(start)}\n')
+    print(f'{total_time(start)}')
 
 # -------------------------------------  8. threading pool executor using executor.map  ------------------------------
 @funcname
 def conc_futures_thrd_with_executor_map():
     '''
         Using concurrent_futures.ThreadPoolExecutor executor map():
-        
-        The 'executor.map()' replaces the 'executor.submit()', and the syntax follows closely to the regular map() function.  
-        
+
+        The 'executor.map()' replaces the 'executor.submit()', and the syntax follows closely to the regular map() function.
+
         In the meantime, for- loop for displaying the thread results show each thread result  is displayed in the order it was started.
 
     '''
@@ -267,14 +275,14 @@ def conc_futures_thrd_with_executor_map():
         for result in results:
             print(result)
 
-    print(f'{total_time(start)}\n')
+    print(f'{total_time(start)}')
 
 
 # --------------------------- 9. threading pool executor using max_workers args passed ------------------------------
 @funcname
 def conc_futures_thrd_with_max_workers(max_workers):
     '''
-        Use concurrent_futures.ThreadPoolExecutor executor map().  For this function, we are passing the 
+        Use concurrent_futures.ThreadPoolExecutor executor map().  For this function, we are passing the
         ""max_workers" argument to the executor object.
 
         NOTE :
@@ -292,7 +300,7 @@ def conc_futures_thrd_with_max_workers(max_workers):
         for result in results:
             print(result)
 
-    print(f'{total_time(start)}\n')
+    print(f'{total_time(start)}')
 
 if __name__ == '__main__':
 
@@ -305,7 +313,7 @@ if __name__ == '__main__':
     ### following three functions use threading.Thread
     run_two_threads_async()                            # running one function concurrently using two threads with start() only
     run_two_threads_with_join()                        # run a function concurrently with two threads using start() and join()
-    run_ten_threads_with_threadlist()                  # threading running 10 threads asynchronously using thread list
+    run_ten_threads_with_threadlist()                  # run 10 threads asynchronously using thread list
 
     ### the recommended way to do threading is to use concurrent.futures.ThreadPoolExecute, with four examples as shown below:
     conc_futures_threading()                            # basic concurrent.futures.ThreadPool
